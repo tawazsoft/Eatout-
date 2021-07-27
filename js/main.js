6dd4209cda4b4df705b20d8af7168e4a5a074e86
Tawanda <tawanda.m@sproutcoding.org>
2021-07-27 23:48:42 +0200

    commit second

--- js/main.js
+++ js/main.js
@@ -6,3 +6,17 @@ $(document).ready(function(){
     });
   });
   
+
+$(document).on('ready', function() {
+
+
+ 
+  $(".regular").slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll:1,
+  });
+  
+  
+})
